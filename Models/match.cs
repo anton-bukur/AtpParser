a0826dfdda44d0a8bc3a946c3aae48d94d581c3c
capybarr@gmail.com <capybarr@gmail.com>
2021-01-06 23:20:20 +0500

    Добавьте файлы проекта.

---
+++ Models/match.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtpParser.Models
+{
+    public class match
+    {
+        public string Atp { get; set; }
+        //public string Location { get; set; }
+        //public string Tournament { get; set; }
+        //public string Date;string Series; string Court; string Surface; string Round; string BestOf; string Winner; string Loser; string WRank; string LRank; string WPts; string LPts; string W1; string L1; string W2; string L2; string W3; string L3; string W4; string L4; string W5; string L5; string Wsets; string Lsets; string Comment; string B365W; string B365L; string PSW; string PSL; string MaxW; string MaxL; string AvgW; string AvgL;
+
+    }
+}
---
+++ proc.cs
@@ -0,0 +1,43 @@
+﻿using AtpParser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AtpParser
+{
+    class proc
+    {
+        public static string GetMatchStatistics(string url)
+        {
+            url = "https://www.tennisexplorer.com/match-detail/?id=1864310";
+            //var proxyHand = new WebProxy
+            //{
+            //    Address = null,
+            //    BypassProxyOnLocal = false,
+            //    UseDefaultCredentials = false
+            //};
+
+            var cookieContainer = new CookieContainer();
+
+            HttpClientHandler handler = new HttpClientHandler
+            {
+                UseProxy = false,
+                AllowAutoRedirect = false,
+                UseCookies = true,
+                CookieContainer = cookieContainer
+            };
+            HttpClient client = new HttpClient(handler);
+            var Result = client.GetAsync(url).Result;
+            var RespText = Result.Content.ReadAsStringAsync().Result;
+            match sMatch  = new match();
+            sMatch.Atp = Regex.Match(RespText,"").Value;
+
+            return RespText;
+        }
+    }
+}
