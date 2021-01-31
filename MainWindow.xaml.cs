5a81831d94dea03806fac2f97a9cc28195f68acd
capybarr@gmail.com <capybarr@gmail.com>
2021-01-31 10:17:36 +0500

    Asian Handicap!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,27 +28,27 @@ namespace AtpParser
             string url = homeUrl + "calendar/atp-men/"+Year+"/";
             List<string> listStat = new List<string>();
 
-            List<string> listTournaments = proc.GetTournaments(url);
-            foreach (string u in listTournaments)
-            {
-                List<string> listMatches = proc.GetMatches(homeUrl + u);
-                if (listMatches == null)
-                {
-                    File.AppendAllText("log.txt", homeUrl + u + "|No matches found" + Environment.NewLine);
-                    continue;
-                }
-                string Tournament = listMatches[listMatches.Count - 1];
-                listMatches.RemoveAt(listMatches.Count - 1);
-                foreach (string s in listMatches)
-                {
-                    string result = proc.GetMatchStatistics(homeUrl + s, Tournament);
-                    listStat.Add(result);
-                }
-                proc.Excel(listStat);
-                break;
-            }
-            //string result = proc.GetMatchStatistics("", "Abu Dhabi - exh. 2020 (UAE)");
-            //listStat.Add(result);
+            //List<string> listTournaments = proc.GetTournaments(url);
+            //foreach (string u in listTournaments)
+            //{
+            //    List<string> listMatches = proc.GetMatches(homeUrl + u);
+            //    if (listMatches == null)
+            //    {
+            //        File.AppendAllText("log.txt", homeUrl + u + "|No matches found" + Environment.NewLine);
+            //        continue;
+            //    }
+            //    string Tournament = listMatches[listMatches.Count - 1];
+            //    listMatches.RemoveAt(listMatches.Count - 1);
+            //    foreach (string s in listMatches)
+            //    {
+            //        string result = proc.GetMatchStatistics(homeUrl + s, Tournament);
+            //        listStat.Add(result);
+            //    }
+            //    proc.Excel(listStat);
+            //    break;
+            //}
+            string result = proc.GetMatchStatistics("", "Abu Dhabi - exh. 2020 (UAE)");
+            listStat.Add(result);
 
 
             InitializeComponent();
